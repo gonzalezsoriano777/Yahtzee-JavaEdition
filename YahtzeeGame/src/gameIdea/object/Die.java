8802c57a0731bd77124205218dcc24b81774cd5c
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-11-16 19:00:23 -0600

    Re running with Yahtzee game will have a beta for it ready by tonight to continue going through it through the week

--- YahtzeeGame/src/gameIdea/object/Die.java
+++ YahtzeeGame/src/gameIdea/object/Die.java
@@ -4,31 +4,6 @@ import java.util.Random;
 
 public class Die {
 	
-	private int diceSides;
-	private int diceValue;
 	
-	// Randomizer for value of dice
-	private Random randomizer = new Random();
-	
-	
-//	public Die(int diceSides) {
-//		this.diceSides = diceSides;
-//	}
-	
-	public Die(int diceSides, int diceValue) {
-		this.diceSides = diceSides;
-		this.setDiceValue(diceValue);
-	}
-	
-	public void Roll() {
-		this.setDiceValue(randomizer.nextInt(diceSides) + 1);
-	}
-	 public int getDiceValue() {
-		 return diceValue;
-	 }
-	
-	public void setDiceValue(int diceValue) {
-		this.diceValue = diceValue;
-	}
 
 }
--- YahtzeeGame/src/gameIdea/object/Hand.java
+++ YahtzeeGame/src/gameIdea/object/Hand.java
@@ -4,8 +4,6 @@ public class Hand {
 	
 	public int YahtzeeGame() {
 		
-		
-		
 	}
 
 }
