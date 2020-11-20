d07043f4590295d368ff57bf53c2985246e210d5
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-11-19 18:59:56 -0600

    Created a dice that rolls based of it's six sides.. chooses a random num from 1 - 6

--- YahtzeeGame/src/gameIdea/object/Die.java
+++ YahtzeeGame/src/gameIdea/object/Die.java
@@ -3,7 +3,22 @@ package gameIdea.object;
 import java.util.Random;
 
 public class Die {
+
+	private int diceSides = 6;
+	private int diceValue = 0;
+
+	public Die() {
+	}
+
+	public Die(int dicesides, int dicevalue) {
+		this.diceSides = dicesides;
+		this.diceValue = dicevalue;
+	}
 	
-	
+	// Once rolled it will randomize the value of the die from range of 1-6
+	public void Roll(Random rand) {
+		int numRolled = rand.nextInt(diceSides) + 1;
+		diceValue = numRolled;
+	}
 
 }
